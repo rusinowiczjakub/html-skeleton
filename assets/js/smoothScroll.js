91bff8303134f0cff5bef84a010242e96845c05b
Kuba Rusinowicz <jakub.rusinowicz@coderslab.pl>
2018-01-12 22:13:18 +0100

    gallery sect & nav collapse

---
+++ assets/js/smoothScroll.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $("li a").on('click', function() {
+
+            $('html, body').animate({
+                scrollTop: $($(this).attr('href')).offset().top
+            }, 800);
+    });
+
+    $("li a").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scroll: $(this).offset()
+        }, 2000, 'linear');
+    });
+    // $('a[href^="#"]').on('click',function (e) {
+    //     e.preventDefault();
+    //
+    //     var target = this.hash;
+    //     var $target = $(target);
+    //
+    //     $('html, body').stop().animate({
+    //         'scrollTop': $target.offset().top
+    //     }, 900, 'swing')
+    // });
+});
