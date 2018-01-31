275440fbdb3785c3e3a0e18d4abcaabda94f36a1
Kuba Rusinowicz <jakub.rusinowicz@coderslab.pl>
2018-02-01 00:52:39 +0100

    Google api placese not working

--- app/js/googleMap.js
+++ app/js/googleMap.js
@@ -1,11 +1,44 @@
+var map;
+var lukasinskiego;
+var service;
+
 function initMap() {
-  var uluru = {lat: 51.7369893, lng: 19.4699291};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  lukasinskiego = {lat: 51.7369893, lng: 19.4699291};
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
-    center: uluru
+    center: lukasinskiego
   });
   var marker = new google.maps.Marker({
-    position: uluru,
+    position: lukasinskiego,
     map: map
+  });
+
+  infowindow = new google.maps.InfoWindow();
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: lukasinskiego,
+    radius: 1000,
+    type: 'store'
+  }, callback);
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    setMap : map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
   });
 }
